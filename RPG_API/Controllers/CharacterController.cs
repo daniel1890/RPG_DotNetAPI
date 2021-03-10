7e0b669d5faa46ac22a29019ae18c03d442eba82
Daniël R <danielroth1890@gmail.com>
2021-03-10 15:36:03 +0100

    Voeg character baseclass toe en character controller, voeg hieraan GetAll en GetSingle methodes toe.

---
+++ RPG_API/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPG_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPG_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Name = "Sam"}
+        };
+
+        [Route("GetAll")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+        [Route("GetSingle")]
+        [HttpGet]
+        public IActionResult GetSingle()
+        {
+            return Ok(characters[0]);
+        }
+    }
+}
